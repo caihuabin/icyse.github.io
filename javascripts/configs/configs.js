71991edf38d4e5e607a33a0049ccf92b1c14db20
Icyse <icyse@sina.com>
2016-04-30 22:00:41 +0800

    4.30

--- javascripts/configs/configs.js
+++ javascripts/configs/configs.js
@@ -1,21 +1,6 @@
 'use strict';
 
 var configs = angular.module('mean.configs', []);
-configs.constant('AUTH_EVENTS', {
-  	loginSuccess: 'auth-login-success',
-  	loginFailed: 'auth-login-failed',
-  	logoutSuccess: 'auth-logout-success',
-  	sessionTimeout: 'auth-session-timeout',
-  	notAuthenticated: 'auth-not-authenticated',
-  	notAuthorized: 'auth-not-authorized'
-});
-
-configs.constant('USER_ROLES', {
-  	all: '*',
-  	admin: 'admin',
-  	editor: 'editor',
-  	guest: 'guest'
-});
 
 configs.constant('DOM_EVENTS', {
     onclick: 'onclick',
@@ -31,8 +16,6 @@ configs.constant('CUSTOM_EVENTS', {
     loading: 'loading',
     loaded: 'loaded',
     loadOver: 'load-over',
-    readFilesSuccess: 'read-files-success',
-    uploadFilesSuccess: 'upload-files-success',
     blogPreviewOpen: 'blog-preview-open',
     blogPreviewClose: 'blog-preview-close'
 });
